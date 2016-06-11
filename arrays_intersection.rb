51505a63d122d6ac63cb13a5d3e57481f9b9d234
davidedelerma <davide.delerma@gmail.com>
2016-06-11 13:41:46 +0100

    initial commit

---
+++ arrays_intersection.rb
@@ -0,0 +1,38 @@
+#creating function for bynary search
+#creating function to find the common elemesnts in the two vectors: 1) sorting the longets array 2) and loop over the short array to search for the common elements (using binary search)
+def binary_search(arr,target)
+  low=0
+  high=arr.length-1
+  while (low<=high)
+    mid=((low+high)/2).floor
+    if (arr[mid] > target)
+      high=mid-1
+    elsif (arr[mid] < target)
+      low=mid+1
+    else
+      return arr[mid]
+    end
+  end
+  return "value not found" 
+end
+
+def find_commonvalues(arr1,arr2)
+  arr1=arr1.uniq
+  arr2=arr2.uniq
+  if (arr1.length<=arr2.length)
+    arr_to_iterate=arr1
+    arr_to_sort=arr2
+  elsif
+    arr_to_iterate=arr2
+    arr_to_sort=arr1
+  end
+  result=[]
+  arr_sorted=arr_to_sort.sort
+  for target in arr_to_iterate
+    comm_value=binary_search(arr_sorted,target)
+    if (comm_value != "value not found")
+      result<<comm_value
+    end
+  end
+  return result
+end
---
+++ spec/arrays_intersection_spec.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require_relative '../arrays_intersection'
+
+class TestArrInt < Minitest::Test
+  def test_binary_search()
+    a=[2,4,6,8,10,12,16,18]
+    result=binary_search(a,4)
+    result1=binary_search(a,16)
+    assert_equal(result,4)
+    assert_equal(result1,16)
+  end 
+  def test_find_commonvalues()
+    a=[2,4,6,8,10,12,16,18]
+    b=[1,2,3,4,5,11,12]
+    result=find_commonvalues(a,b)
+    assert_equal(result,[2,4,12])
+  end 
+end
